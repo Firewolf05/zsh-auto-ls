9e09a8817fac6c05336dd3d413e571f6971cad40
George <77410335+Firewolf05@users.noreply.github.com>
2022-07-31 23:11:34 +0300

    Add files via upload

--- auto-ls.zsh
+++ auto-ls.zsh
@@ -1,18 +1,11 @@
 auto_ls() {
 	emulate -L zsh
-  #if [[ ! $(pwd) == "$HOME/.config" ]]; then 
-  #echo
-  #fi
-  if command -v colorls &>/dev/null; then
-    colorls -A --group-directories-first
+  if [[ $(pwd) == "$HOME/.trash" ]]; then 
+    exa --all --icons
   elif [[ ! $(pwd) == "$HOME/.config" ]]; then 
-    ls
-  #  echo
+    exa --color=always --group-directories-first --icons
   fi
 }
-
-# Check if auto-ls has already been added to the chpwd_functions array. This
-# ensures that resourcing the zshrc file doesnt cause ls to be run twice.
 if [[ ! " ${chpwd_functions[*]} " =~ "auto_ls" ]]; then
   chpwd_functions=(auto_ls $chpwd_functions)
 fi
